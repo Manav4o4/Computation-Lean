8a9d3689917c5b4a8ccb1c1bc067e443703ac61d
Manav Bajaj <manavbajaj253@gmail.com>
2024-09-29 22:11:27 -0400

    Test file

---
+++ test_file.lean
@@ -0,0 +1,15 @@
+#eval Lean.versionString --#eval runs the code
+
+#eval 1+1 -- Basic Computation
+
+#eval (1 - 2: Int) -- Can assign types of the output
+
+#check 2^34 --Gives the type, natural #s
+
+#eval String.append "hello, " "Lean" -- String ops
+
+#check String.append "hello, " "Lean" -- Checks the type
+
+#eval String.append "Great " (String.append "oak " "tree") -- Nested functions
+
+#eval String.append "it is " (if 1 > 2 then "yes" else "no")
